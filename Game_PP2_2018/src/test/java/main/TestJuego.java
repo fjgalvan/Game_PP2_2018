0c92780ef820978069cefa4d935d458a302caea3
fjgalvan <fjgalvanx87@gmail.com>
2018-04-17 17:30:43 -0300

    Test Juego y de Main

--- Game_PP2_2018/src/test/java/main/TestJuego.java
+++ Game_PP2_2018/src/test/java/main/TestJuego.java
@@ -1,10 +1,48 @@
 package main;
 
+import grafico.Pantalla;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
 import org.junit.Test;
 
+import control.Teclado;
+
 public class TestJuego {
+	@SuppressWarnings("unused")
 	@Test
-	public void testHojaSprite() {
+	public void testJuego() {
+		Thread thread; // agregamos el volatile porque estamos usando 2 threads
+		boolean enFuncionamiento = false;
+		int aps = 0; // actualizaciones por segundo
+		int fps = 0; // frames por segundo
+		Teclado teclado;
+		int x = 0;
+		int y = 0;
+		String CONTADOR_APS = "";
+		String CONTADOR_FPS = "";
+		Pantalla pantalla;
+		BufferedImage imagen = new BufferedImage(Constantes.ANCHO, Constantes.ALTO,
+				BufferedImage.TYPE_INT_RGB);
+		int [] pixeles = ((DataBufferInt) imagen.getRaster().getDataBuffer()).getData();
+		final ImageIcon icono = new ImageIcon (Juego.class.getResource("/icono/icono.png"));
+		
+		// ventanaJuego
+		final long serialVersionUID = 1L;
+		JFrame ventana;
+		// ventanaJuego
+		
+		Juego juego= new Juego();
+		
+		juego.iniciar();
+		juego.detener();
+		juego.actualizar();
+		juego.mostrar();
+		juego.run();
 		
 	}
 }
--- Game_PP2_2018/src/test/java/main/TestMain.java
+++ Game_PP2_2018/src/test/java/main/TestMain.java
@@ -1,5 +1,11 @@
 package main;
 
-public class TestMain {
+import org.junit.Test;
 
+public class TestMain {
+	@Test
+	public void testMain() {
+		Juego juego = new Juego();
+		juego.iniciar();
+	}
 }
