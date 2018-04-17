2df7f3f3b00acb8d7fb8daf41c97c02b9d197c47
fjgalvan <fjgalvanx87@gmail.com>
2018-04-17 17:41:58 -0300

    test Juego

--- Game_PP2_2018/src/test/java/main/TestJuego.java
+++ Game_PP2_2018/src/test/java/main/TestJuego.java
@@ -2,6 +2,7 @@ package main;
 
 import grafico.Pantalla;
 
+import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
 
@@ -20,7 +21,7 @@ public class TestJuego {
 		boolean enFuncionamiento = false;
 		int aps = 0; // actualizaciones por segundo
 		int fps = 0; // frames por segundo
-		Teclado teclado;
+		//Teclado teclado;
 		int x = 0;
 		int y = 0;
 		String CONTADOR_APS = "";
@@ -36,7 +37,24 @@ public class TestJuego {
 		JFrame ventana;
 		// ventanaJuego
 		
+		
+		
 		Juego juego= new Juego();
+		
+		final int numeroTeclas = 120;
+		final boolean[] teclas = new boolean[numeroTeclas];
+		boolean arriba= teclas[KeyEvent.VK_W]; // la tecla arriba equivale a pulsar W;
+		boolean abajo= teclas[KeyEvent.VK_S]; // la tecla arriba equivale a pulsar S
+		boolean izquierda= teclas[KeyEvent.VK_A]; // la tecla arriba equivale a pulsar A
+		boolean derecha= teclas[KeyEvent.VK_D]; // la tecla arriba equivale a pulsar D
+		Teclado teclado= new Teclado();
+		teclado.arriba= arriba;
+		teclado.abajo= abajo;
+		teclado.izquierda= izquierda;
+		teclado.derecha= derecha;
+		
+		teclado.actualizar();
+		
 		
 		juego.iniciar();
 		juego.detener();
