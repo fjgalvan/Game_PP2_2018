d92428a2999884d297df0f819bea7c401d1b5dbf
fjgalvan <fjgalvanx87@gmail.com>
2018-04-17 17:21:11 -0300

    Hello World!

---
+++ Game_PP2_2018/src/main/java/arcade/Game_PP2_2018/App.java
@@ -0,0 +1,14 @@
+package arcade.Game_PP2_2018;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+    }
+}
