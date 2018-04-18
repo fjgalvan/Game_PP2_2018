8fc4f7de6b3d26337bd1c0cbf8a5beb042b72821
Lautaro <lautydb@yahoo.com>
2018-04-18 18:28:33 -0300

    dibujador

---
+++ Game_PP2_2018/src/main/java/modelo/ObjectGraphic.java
@@ -0,0 +1,50 @@
+package modelo;
+
+import java.awt.Point;
+import java.awt.image.BufferedImage;
+
+public class ObjectGraphic {
+	protected Point coordinate;
+	protected Point size;
+	protected BufferedImage image;
+
+	public ObjectGraphic(Point coordinate, Point size, BufferedImage image) {
+		this.coordinate = coordinate;
+		this.size = size;
+		this.image = image;
+	}
+
+	public ObjectGraphic(Point coordinate, Point size) {
+		this.coordinate = coordinate;
+		this.size = size;
+	}
+
+	public ObjectGraphic(BufferedImage image) {
+		this.image = image;
+	}
+
+	public BufferedImage getImage() {
+		return this.image;
+	}
+
+	public Point getCoordinate() {
+		return coordinate;
+	}
+
+	public void setCoordinate(Point coordinate) {
+		this.coordinate = coordinate;
+	}
+
+	public Point getSize() {
+		return size;
+	}
+
+	public void setSize(Point size) {
+		this.size = size;
+	}
+
+	public void setImage(BufferedImage image) {
+		this.image = image;
+	}
+
+}
