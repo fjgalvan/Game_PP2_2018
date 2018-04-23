e155d8123212996ea615991e6902963fa92a8dcb
fjgalvan <fjgalvanx87@gmail.com>
2018-04-23 20:32:59 -0300

    test clase Properties

--- Game_PP2_2018/src/test/java/claseProperties/TestEleccionMenu.java
+++ Game_PP2_2018/src/test/java/claseProperties/TestEleccionMenu.java
@@ -1,5 +1,25 @@
 package claseProperties;
 
-public class TestEleccionMenu {
+import org.junit.Test;
 
+public class TestEleccionMenu {
+	@SuppressWarnings("unused")
+	@Test
+	public void testEleccionMenu() {
+		String tecla_movimiento_arriba="w";
+		String tecla_movimiento_abajo="s";
+		String tecla_movimiento_izquierda="a";
+		String tecla_movimiento_derecha="d";
+		String tecla_disparo="space";
+		int personaje1_posX=100;
+		int personaje1_posY=100;
+		int personaje1_height=40;
+		int personaje1_width=40;
+		String personaje1_string_imagen="Image";
+		
+		EleccionMenu menu= new EleccionMenu(tecla_movimiento_arriba, tecla_movimiento_abajo, tecla_movimiento_izquierda,
+				tecla_movimiento_derecha, tecla_disparo, personaje1_posX, personaje1_posY, personaje1_height, 
+				personaje1_width, personaje1_string_imagen);
+		
+	}
 }
